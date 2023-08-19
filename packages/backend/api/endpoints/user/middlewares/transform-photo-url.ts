3c3661c21f00b7252a06d51d3150c5c6bbd00bac
pmartinsdev <martins20.tech@gmail.com>
2023-08-19 19:26:05 -0300

    feat: add link into followers and followees data

--- packages/backend/api/endpoints/user/middlewares/transform-photo-url.ts
+++ packages/backend/api/endpoints/user/middlewares/transform-photo-url.ts
@@ -2,20 +2,25 @@ import { Request, Response } from "express";
 
 import { ProfileWithPhotoUrl } from "../dtos/profile-with-photo-url";
 import { GLOBAL_PREFIX, PORT } from "@infrastructures/constants/server";
+import { Follow } from "@domains/entities/follow";
+
+const getPhotoLink = (req: Request, filename?: string | null): string | null => {
+  return filename ? `${req.protocol}://${req.hostname}:${PORT}${GLOBAL_PREFIX}/images/${filename}` : null
+}
 
 export const transformPhotoResponse = (req: Request, res: Response) => {
   const user: any = res.locals.user;
 
-  const { photo_filename, ...data } = user
+  const { photo_filename, followers, followees, ...data } = user
 
   const response: ProfileWithPhotoUrl = {
     id: user.id,
     name: user.name,
     email: user.email,
     username: user.username,
-    photo_url: user.photo_filename
-      ? `${req.protocol}://${req.hostname}:${PORT}${GLOBAL_PREFIX}/images/${user.photo_filename}`
-      : '',
+    photo_url: getPhotoLink(req, user.photo_filename),
+    followers: followers.map((follow: Follow) => ({ ...follow, follower_photo: getPhotoLink(req, follow.follower_photo), following_photo: getPhotoLink(req, follow.following_photo) })),
+    followees: followers.map((follow: Follow) => ({ ...follow, follower_photo: getPhotoLink(req, follow.follower_photo), following_photo: getPhotoLink(req, follow.following_photo) })),
     ...data
   };
 
